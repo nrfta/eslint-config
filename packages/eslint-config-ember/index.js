fdf353d7b820b09f16353180e2634fff1ecf4c40
Josemar Luedke <josemarluedke@gmail.com>
2024-01-21 17:40:09 -0800

    chore: update eslint and ember deps, add gts,gjs parser options

--- packages/eslint-config-ember/index.js
+++ packages/eslint-config-ember/index.js
@@ -1,12 +1,8 @@
 'use strict';
 
 module.exports = {
-  plugins: ['ember', 'ember-best-practices'],
-  extends: [
-    '@underline',
-    'plugin:ember/recommended',
-    'plugin:ember-best-practices/recommended'
-  ],
+  plugins: ['ember'],
+  extends: ['@underline', 'plugin:ember/recommended'],
   rules: {
     'ember/no-deeply-nested-dependent-keys-with-each': 'error',
     'ember/no-invalid-debug-function-arguments': 'error',
@@ -18,31 +14,23 @@ module.exports = {
     'ember/avoid-leaking-state-in-ember-objects': 'error',
     'ember/no-unnecessary-route-path-option': 'error',
     'ember/no-test-and-then': 'error',
-
     'ember/no-empty-glimmer-component-classes': 'off',
-
-    // Same thing
-    'ember-best-practices/no-observers': 'off',
     'ember/no-observers': 'error',
-
-    // Same thing
-    'ember-best-practices/no-attrs-snapshot': 'off',
     'ember/no-attrs-snapshot': 'error',
-
-    // Same thing
-    'ember-best-practices/no-attrs': 'off',
     'ember/no-attrs-in-components': 'error',
-
-    // Same thing
-    'ember-best-practices/no-broken-super-chain': 'off',
-
-    // Same thing
-    'ember-best-practices/no-global-jquery': 'off',
     'ember/no-global-jquery': 'error',
-
-    // Same thing
-    'ember-best-practices/no-send-action': 'off',
     'ember/closure-actions': 'error'
   },
-  overrides: []
+  overrides: [
+    {
+      files: ['**/*.gjs'],
+      parser: 'ember-eslint-parser',
+      plugins: ['ember'],
+      extends: [
+        '@underline',
+        'plugin:ember/recommended',
+        'plugin:ember/recommended-gjs'
+      ]
+    }
+  ]
 };
